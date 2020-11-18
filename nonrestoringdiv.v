c18d228ff784f803ad6d101f31bbc5b9d61ce94e
Lukas Pettersson <lucke97@gmail.com>
2020-11-17 19:16:25 -0800

    working division with start and end bits

--- nonrestoringdiv.v
+++ nonrestoringdiv.v
@@ -3,72 +3,76 @@ module nonrestoringdiv(
 	input  [511 : 0] Q, // Q is quotient, but since it's also an input, we're gonna use another variable for quotient
 	input  [511 : 0] M, // M is divisor
 	input  [511 : 0] A, // A is accumulator and also remainder, same deal as Q
+	input start,
 	output [511 : 0] Q_out, //Quotient
-	output [511 : 0] R //Remainder
-	);
+	output [511 : 0] R, //Remainder
+	output reg done);
 
 reg [511 : 0] qReg, mReg, aReg;
-reg flag = 1'b1;
-reg [511 : 0] count = 511'd512;
-integer i = 0;
+reg flag;
+reg [511 : 0] count;
+reg state = 0;
+
 /*
 initial begin
 	qReg = Q;
 	mReg = M;
 	aReg = A;
 	flag = 1;
-end */
-
+end
+*/
 assign Q_out = qReg;
 assign R = aReg;
-
-// TODO: Fix bits of count register for optimization, doesn't have to be 32.
-
 always @ (posedge clk)
 	begin
-		if(i == 0)
+		case(state)
+			0:
+			if(start)
 			begin
 				qReg = Q;
 				mReg = M;
 				aReg = A;
-				i = 1;
+				done = 0;
+				count = 512'd512;
+				state = 1;
+				flag = 1;
 			end
-
-		if(count > 0) //while(count)
-			begin
-			aReg = {aReg[510 : 0] , qReg[511]};
-
-			if(flag == 1'b1) begin aReg = aReg - mReg; end
-			else begin aReg = aReg + mReg; end
-
-			if(aReg[511] == 1'b1)
+			1:
+			if(count > 0) //while(count)
 				begin
-					qReg = {qReg[510 : 0], 1'b0};
-					flag = 1'b0;
-				end
+				aReg = {aReg[510 : 0] , qReg[511]};
+
+				if(flag == 1'b1) begin aReg = aReg - mReg; end
+				else begin aReg = aReg + mReg; end
+
+				if(aReg[511] == 1'b1)
+					begin
+						qReg = {qReg[510 : 0], 1'b0};
+						flag = 1'b0;
+					end
+					else
+					begin
+						qReg = {qReg[510 : 0], 1'b1};
+						flag = 1'b1;
+					end
+					count = count - 1;
+				end //end if count
 				else
 				begin
-					qReg = {qReg[510 : 0], 1'b1};
-					flag = 1'b1;
+				if(aReg[511] == 1'b1) begin
+					aReg = aReg + mReg;
+					mReg = mReg;
+					qReg = qReg;
 				end
-				count = count - 1;
-			end //end if count
-			else
-			begin
-			if(aReg[511] == 1'b1) begin
-				aReg = aReg + mReg;
-				mReg = mReg;
-				qReg = qReg;
-			end
-			else begin
-				qReg = qReg;
-				aReg = aReg;
-				mReg = mReg;
-			end
-			end
+				else begin
+					qReg = qReg;
+					aReg = aReg;
+					mReg = mReg;
 
-	end //end of posedge clk
-
-
+				end
+				done = 1;
+				end
+		endcase
+	end
 
 endmodule
--- nonrestoringdiv_tb.v
+++ nonrestoringdiv_tb.v
@@ -3,12 +3,13 @@ module nonrestoringdiv_tb();
 
 
 reg clk;
+reg start;
 reg  [511 : 0] Q; // Q is quotient, but since it's also an reg, we're gonna use another variable for quotient
 reg  [511 : 0] M; // M is divisor
 reg  [511 : 0] A; // A is accumulator and also remainder, same deal as Q
 wire [511 : 0] Q_out; //Quotient
 wire [511 : 0] R; //Remainder
-
+wire done;
 //512 bit max
 //13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084095
 
@@ -17,6 +18,8 @@ initial begin
 	Q = 512'd13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858181546824682753882811946569946433649006084095; //Dividend
 	A = 512'd0;
 	M = 512'd56482457212336265846843516806813506813508168304698446384098494698465413057468478409807870604; //Divisor
+	#50
+	start = 1;
 end
 
 always #25 clk = ~clk;
@@ -30,7 +33,9 @@ always #25 clk = ~clk;
 	.M(M), // M is divisor
 	.A(A), // A is accumulator and also remainder, same deal as Q
 	.Q_out(Q_out), //Quotient
-	.R(R) //Remainder
+	.R(R), //Remainder
+	.start(start),
+	.done(done)
 	);
 
 endmodule
