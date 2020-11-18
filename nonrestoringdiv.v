22673aabcac370d0a55dbc4e8c3fce7eba7418bc
Lukas Pettersson <lucke97@gmail.com>
2020-11-17 16:49:26 -0800

    working division

--- nonrestoringdiv.v
+++ nonrestoringdiv.v
@@ -1,15 +1,15 @@
 module nonrestoringdiv(
 	input clk,
-	input  [31 : 0] Q, // Q is quotient, but since it's also an input, we're gonna use another variable for quotient
-	input  [31 : 0] M, // M is divisor
-	input  [31 : 0] A, // A is accumulator and also remainder, same deal as Q
-	output [31 : 0] Q_out, //Quotient
-	output [31 : 0] R //Remainder
+	input  [511 : 0] Q, // Q is quotient, but since it's also an input, we're gonna use another variable for quotient
+	input  [511 : 0] M, // M is divisor
+	input  [511 : 0] A, // A is accumulator and also remainder, same deal as Q
+	output [511 : 0] Q_out, //Quotient
+	output [511 : 0] R //Remainder
 	);
 
-reg [31 : 0] qReg, mReg, aReg;
+reg [511 : 0] qReg, mReg, aReg;
 reg flag = 1'b1;
-reg [31 : 0] count = 32'd32;
+reg [511 : 0] count = 511'd512;
 integer i = 0;
 /*
 initial begin
@@ -34,34 +34,41 @@ always @ (posedge clk)
 				i = 1;
 			end
 
-		if(count > 4'd32) //while(count)
+		if(count > 0) //while(count)
 			begin
-			aReg = {aReg[30 : 0] , qReg[31]};
+			aReg = {aReg[510 : 0] , qReg[511]};
 
 			if(flag == 1'b1) begin aReg = aReg - mReg; end
 			else begin aReg = aReg + mReg; end
 
-			if(aReg[31] == 1'b1)
+			if(aReg[511] == 1'b1)
 				begin
-					qReg = {qReg[30 : 0], 1'b0};
+					qReg = {qReg[510 : 0], 1'b0};
 					flag = 1'b0;
 				end
-				else 
+				else
 				begin
-					qReg = {qReg[30 : 0], 1'b1};
+					qReg = {qReg[510 : 0], 1'b1};
 					flag = 1'b1;
 				end
 				count = count - 1;
 			end //end if count
-			else 
+			else
 			begin
+			if(aReg[511] == 1'b1) begin
+				aReg = aReg + mReg;
+				mReg = mReg;
+				qReg = qReg;
+			end
+			else begin
 				qReg = qReg;
 				aReg = aReg;
 				mReg = mReg;
+			end
 			end
 
 	end //end of posedge clk
 
 
 
-endmodule 
+endmodule
--- nonrestoringdiv_tb.v
+++ nonrestoringdiv_tb.v
@@ -3,17 +3,20 @@ module nonrestoringdiv_tb();
 
 
 reg clk;
-reg  [31 : 0] Q; // Q is quotient, but since it's also an reg, we're gonna use another variable for quotient
-reg  [31 : 0] M; // M is divisor
-reg  [31 : 0] A; // A is accumulator and also remainder, same deal as Q
-wire [31 : 0] Q_out; //Quotient
-wire [31 : 0] R; //Remainder
+reg  [511 : 0] Q; // Q is quotient, but since it's also an reg, we're gonna use another variable for quotient
+reg  [511 : 0] M; // M is divisor
+reg  [511 : 0] A; // A is accumulator and also remainder, same deal as Q
+wire [511 : 0] Q_out; //Quotient
+wire [511 : 0] R; //Remainder
+
+//512 bit max
+//13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858186486050853753882811946569946433649006084095
 
 initial begin
 	clk = 0;
-	Q = 32'd61; //Dividend
-	A = 32'b0000;
-	M = 32'd2; //Divisor
+	Q = 512'd13407807929942597099574024998205846127479365820592393377723561443721764030073546976801874298166903427690031858181546824682753882811946569946433649006084095; //Dividend
+	A = 512'd0;
+	M = 512'd56482457212336265846843516806813506813508168304698446384098494698465413057468478409807870604; //Divisor
 end
 
 always #25 clk = ~clk;
