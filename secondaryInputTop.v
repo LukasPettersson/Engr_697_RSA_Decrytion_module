2766b2b175986336077a011b66fa5b6e604e206d
Lukas Pettersson <lucke97@gmail.com>
2020-11-23 10:34:49 -0800

    wrote secondary input top module, still needs work

--- secondaryInputTop.v
+++ secondaryInputTop.v
@@ -1,23 +1,65 @@
-module secondaryInputTop();
+module secondaryInputTop( input start, clk,
+                          input [1023:0] n,
+                          output [31:0] n0p,
+                          output [1023:0] r, t);
 
 
 
 /*
-all:
-start
 
-n0prime:
-input p, q
-output t, qinv
-
-r:
-input p, q
-output: 
-
-insantiate n0prime, r, t
+t:
 */
+reg start, clk;
+reg n0prime_flag,r_t_flag;
 
-always (@posedge clk)
+/* placeholder for when the r/t module is done
+r_t rt0(
 
 
+  );
+  */
+//which one is right output?
+n0prime n0(   .n(n),
+              .n0prime(n0p),
+              .start(start),
+              .clk(clk)
+              .done(n0prime_done));
+
+always(@posedge clk) begin
+  if(start) begin
+    r_t_flag = 0;
+    n0prime_flag = 0;
+    state = 0;
+  end
+end
+//janky?
+always (@posedge clk) begin
+  if(n0prime_done) begin
+    n0prime_flag = 1;
+  end
+end
+always (@posedge clk) begin
+  if(r_t_done) begin
+    r_t_flag = 1;
+  end
+end
+always (@posedge clk) begin
+
+  case(state)
+  0: begin
+    start = 1;
+    state = 1;
+  end
+  1:begin
+    start = 0;
+    if(n0prime_flag && r_t_flag) begin
+      state = 2;
+    end
+  end
+  2: //output all the values from the submodules into the top modules
+  3:
+  4:
+
+  endcase
+end
 endmodule
